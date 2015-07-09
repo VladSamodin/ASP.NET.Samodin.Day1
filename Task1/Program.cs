ddfe5e7afb05ab5567c6bdd62663f0b0660e142e
VladSamodin <samodin.vladislav@gmail.com>
2015-07-09 15:48:45 +0200

    add test calculation nth root

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,39 +1,36 @@
 ﻿using System;
-using Task2.SortJaggedArray;
 
 namespace Task1
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine(@"5\/6 = {0}", Task1.Math.NthRoot(20000, 2, 0.0000001));
-            Console.WriteLine(@"5\/6 = {0}", System.Math.Pow(20000, 1.0 / 2));
-            //System.Math.Pow()
-            //Task1.Math.NthRoot(5, 2, 4)
-
-            int[][] array = new int[5][] {new int[3] {2, 5, 3}, 
-                                          new int[4] {0, -5, 3 , 1},
-                                          new int[2] {10, 16},
-                                          new int[3] {1, 2, 3},
-                                          new int[4] {4, -3, 1, 2}};
-
-            Console.WriteLine("Source array:");
-            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine("Введите число");
+            double number;
+            while (!Double.TryParse(Console.ReadLine(), out number))
             {
-                for (int j = 0; j < array[i].Length; j++)
-                    Console.Write("{0, 3} ", array[i][j]);
-                Console.WriteLine();
+                Console.WriteLine("Введено некорректное число. Повторите ввод");
             }
-            JaggedArray.Sort(array, JaggedArray.TypeSort.BySumElements, JaggedArray.OrderSort.Desc);
-            Console.WriteLine("Sorted array:");
-            for (int i = 0; i < array.Length; i++)
+
+            Console.WriteLine("Введите степень корня");
+            int degreeRoot;
+            while (!int.TryParse(Console.ReadLine(), out degreeRoot) || degreeRoot < 1)
             {
-                for (int j = 0; j < array[i].Length; j++)
-                    Console.Write("{0, 3} ", array[i][j]);
-                Console.WriteLine();
+                Console.WriteLine("Введено некорректный показатель степени корня. Повторите ввод");
             }
+
+            Console.WriteLine("Введите требуемую точность");
+            double precision;
+            while (!Double.TryParse(Console.ReadLine(), out precision))
+            {
+                Console.WriteLine("Введено некорректное число. Повторите ввод");
+            }
+
+            Console.WriteLine("Корень {0}-й стеени из числа {1}, вычисленный с точностью до {2}", degreeRoot, number, precision);
+            Console.WriteLine(Task1.Math.NthRoot(number, degreeRoot, precision));
+            Console.WriteLine("Корень {0}-й стеени из числа {1}, вычисленный методом Math.Pow", degreeRoot, number);
+            Console.WriteLine(System.Math.Pow(number, 1.0 / degreeRoot));
         }
     }
 }
